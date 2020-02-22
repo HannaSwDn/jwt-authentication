ff75e32877f4bdd534f72b392826f9ef22b7da7c
hanna_nyman <hannanyman.sk@gmail.com>
2020-02-22 19:51:05 +0100

    Adds components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
+import Confidential from './Confidential';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route path="/" exact component={Home} />
-      </div>
+      <Switch>
+        <div className="App">
+          <Route path="/" exact component={Home} />
+          <Route path="/confidential" component={Confidential} />
+        </div>
+      </Switch>
     </Router>
   );
 }
---
+++ src/Confidential.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Confidential() {
+  return (
+    <div className="Confidential">
+      <h1>Confidential Page</h1>
+    </div>
+  );
+}
+
+export default Confidential;
