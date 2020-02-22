9e32e1b7b173806657506d7ec525cd9d211a80c0
hanna_nyman <hannanyman.sk@gmail.com>
2020-02-22 19:14:53 +0100

    Adds React Router and Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={Home} />
+      </div>
+    </Router>
   );
 }
 
